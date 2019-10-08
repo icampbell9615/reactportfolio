a7a5b73a9bf1917ea90608f7243cfec94db4f947
Campbell,Ian <icampbell9615@gmail.com>
2019-10-08 10:00:07 -0500

    heroku

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(express.static('../client/build'));
+
+app.listen(PORT, ()=> console.log("Listening on :%s", PORT))
