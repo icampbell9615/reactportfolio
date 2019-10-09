4212b2e9cf7522d22cf3e9ba4fad3f0e0fe45bcd
Campbell,Ian <icampbell9615@gmail.com>
2019-10-08 19:13:47 -0500

    update my app

--- client/src/pages/Landingpage.js
+++ client/src/pages/Landingpage.js
@@ -10,20 +10,6 @@ const styles = {
     }
 }
 
-$(function() {
-	// Get page title
-  	var pageTitle = $("title").text();
-
-	// Change page title on blur
-	$(window).blur(function() {
-	  $("title").text("Don't forget to read this...");
-	});
-
-	// Change page title back on focus
-	$(window).focus(function() {
-	  $("title").text("Ian Campbell");
-	});
-});
 
 
 
