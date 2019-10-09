bc1854fbfd643c533fe5f623c7031360439318ba
Campbell,Ian <icampbell9615@gmail.com>
2019-10-08 19:08:26 -0500

    update my app

--- client/src/pages/Landingpage.js
+++ client/src/pages/Landingpage.js
@@ -10,6 +10,21 @@ const styles = {
     }
 }
 
+$(function() {
+	// Get page title
+  	var pageTitle = $("title").text();
+
+	// Change page title on blur
+	$(window).blur(function() {
+	  $("title").text("Don't forget to read this...");
+	});
+
+	// Change page title back on focus
+	$(window).focus(function() {
+	  $("title").text(Ian Campbell);
+	});
+});
+
 
 
 class Landingpage extends Component {
